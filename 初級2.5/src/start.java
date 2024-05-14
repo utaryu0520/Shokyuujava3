94b72ca668162dd2d5ac0ab1c29804ac3ccdaec8
RYUSEI <utaryu5020@gmail.com>
2024-05-14 11:07:52 +0900

    5/14 ２時間目

--- 初級2.5/src/start.java
+++ 初級2.5/src/start.java
@@ -2,10 +2,7 @@
 public class start {
 
 	public static void main(String[] args) {
-		MyFrame frame1 = new MyFrame();
-		frame1.setLocation(350,200);
-		
-		MyFrame frame2 = new MyFrame();
-		frame2.setLocation(450,200);
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 = new RectTest();
 	}
 }
