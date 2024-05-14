5d3617540f5abb8b138b4affabbc9843da7518e7
RYUSEI <utaryu5020@gmail.com>
2024-05-14 12:18:56 +0900

    章題3.4

--- 初級2.5/src/RectTest.java
+++ 初級2.5/src/RectTest.java
@@ -2,11 +2,6 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
-		setColor(255,0,0);
-		fillRect(30,40,50,100);
-		setColor(0,255,0);
-	    fillRect(130,40,50,100);
-	    setColor(0,0,255);
-	    fillRect(30,200,150,10);
+		
 	}
 }
