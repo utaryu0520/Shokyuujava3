63aeaff897c5a4b5ba9bcb90973570e30bf7df16
RYUSEI <utaryu5020@gmail.com>
2024-05-20 11:45:53 +0900

    4-3 リスト

--- 初級2.5/src/RectLoop.java
+++ 初級2.5/src/RectLoop.java
@@ -3,7 +3,8 @@ public class RectLoop extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
 		int x = 30;
-		while(x <= 210) {
+		for(int i = 0; i < 15; i ++) {
+			System.out.println(x);
 			fillRect(x,80,10,100);
 			x += 20;
 		}
