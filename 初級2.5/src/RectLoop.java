067cb2d3c6bc5a49b1f30a5e9d08865af6ec7c32
RYUSEI <utaryu5020@gmail.com>
2024-05-20 11:08:56 +0900

    4-2 練習

--- 初級2.5/src/RectLoop.java
+++ 初級2.5/src/RectLoop.java
@@ -3,8 +3,7 @@ public class RectLoop extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
 		int x = 30;
-		while(x <= 170) {
-			System.out.println(x);
+		while(x <= 210) {
 			fillRect(x,80,10,100);
 			x += 20;
 		}
