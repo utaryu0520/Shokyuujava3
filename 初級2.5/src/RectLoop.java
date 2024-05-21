2ee886ec2f75a51c15b4bedede342cd7ed82d61f
RYUSEI <utaryu5020@gmail.com>
2024-05-21 11:00:31 +0900

    追加課題その２

--- 初級2.5/src/RectLoop.java
+++ 初級2.5/src/RectLoop.java
@@ -2,10 +2,15 @@
 public class RectLoop extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
-		int x = 5;
+		int x = 100;
+		int y = 50;
+		int z = 30;
 		for(int i = 0; i < 10; i ++) {
-			fillRect(x,x,x + 5,x + 5);
-			x = x + x + 5;
+			fillRect(z,40,10,x);
+			fillRect(z,240 - y,10,y);
+		    x -= 10;
+		    y += 10;
+		    z += 30;
 		}
 	}
 }
