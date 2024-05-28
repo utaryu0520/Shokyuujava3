1a5f85c167f4fd3bb77e8580e112592d7fd8b3b6
RYUSEI <utaryu5020@gmail.com>
2024-05-28 10:19:26 +0900

    6-2 簡単に描けるようにする

--- 初級2.5/src/DisplayFaceObject.java
+++ 初級2.5/src/DisplayFaceObject.java
@@ -2,10 +2,14 @@
 public class DisplayFaceObject  extends MyFrame{
 	public void run() {
 		Face face1 = new Face(50,50);
-		face1.draw(this);
 		Face face2 = new Face(200,100);
-		face2.draw(this);
-		Face face3 = new Face(150,200);
-		face3.draw(this);
+		for(int i = 0; i < 30; i++) {
+			clear();
+			face1.draw(this);
+			face2.draw(this);
+			face1.x += 10;
+			face2.x += 10;
+			sleep(0.1);
+		}
 	}
 }
