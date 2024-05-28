7a56c2f1b02a228ca8e5222906ece971ea0322a9
RYUSEI <utaryu5020@gmail.com>
2024-05-28 10:14:29 +0900

    6-2 確認問題

--- 初級2.5/src/DisplayFaceObject.java
+++ 初級2.5/src/DisplayFaceObject.java
@@ -1,4 +1,11 @@
 
-public class DisplayFaceObject {
-
+public class DisplayFaceObject  extends MyFrame{
+	public void run() {
+		Face face1 = new Face(50,50);
+		face1.draw(this);
+		Face face2 = new Face(200,100);
+		face2.draw(this);
+		Face face3 = new Face(150,200);
+		face3.draw(this);
+	}
 }
--- 初級2.5/src/start.java
+++ 初級2.5/src/start.java
@@ -3,6 +3,6 @@ public class start {
 
 	public static void main(String[] args) {
 		System.out.println("プログラムを実行します");
-		MyFrame frame1 = new DisplayFace();
+		MyFrame frame1 = new DisplayFaceObject();
 	}
 }
