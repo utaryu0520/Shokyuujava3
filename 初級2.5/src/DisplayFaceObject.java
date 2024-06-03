57117a45de2432e5c9f2939cc07cc1acb2b68b22
RYUSEI <utaryu5020@gmail.com>
2024-06-03 09:55:38 +0900

    7-1 練習問題

--- 初級2.5/src/DisplayFaceObject.java
+++ 初級2.5/src/DisplayFaceObject.java
@@ -1,14 +1,16 @@
 
 public class DisplayFaceObject  extends MyFrame{
 	public void run() {
-		Face[] faces = new Face[3];
-	    faces[0] = new Face(50,50,10,5);
-	    faces[1] = new Face(200,100,-10,-5);
-		faces[2] = new Face(0,100,5,0);
+		Face[] faces = new Face[5];
+	    faces[0] = new Face(50,50,1,0);
+	    faces[1] = new Face(110,110,2,0);
+		faces[2] = new Face(160,160,3,0);
+		faces[3] = new Face(220,220,4,0);
+		faces[4] = new Face(280,280,5,0);
 		for(int i = 0; i < 30; i++) {
 			clear();
-			for(int j = 0; j < 3; j++) {
-				faces[j].draw(this);
+			for(int j = 0; j < faces.length; j++) {
+				faces[j].Car(this);
 				faces[j].move();
 			}
 			sleep(0.1);
--- 初級2.5/src/Face.java
+++ 初級2.5/src/Face.java
@@ -7,11 +7,20 @@ public class Face {
 		this.vx = vx;
 		this.vy = vy;
 	}
+	
+	public void Car(MyFrame frame){
+		frame.fillRect(x, y, 30, 15);
+		frame.fillRect(x - 15, y + 15, 60, 15);
+		frame.fillOval(x -7.5, y + 30, 15, 15);
+		frame.fillOval(x + 22.5, y + 30, 15, 15);
+	}
+	
 	public void draw(MyFrame frame) {
 		frame.fillOval(x,y,50,100);
 		frame.fillOval(x + 100,y,50,100);
 		frame.fillRect(x,y + 160,150,10);
 	}
+	
 	public void move() {
 		x += vx;
 		y += vy;
