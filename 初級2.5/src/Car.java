1aabdd299ce106d5c1b6a801334b194ec8d19c90
RYUSEI <utaryu5020@gmail.com>
2024-06-10 10:43:56 +0900

    8-3 共通する性質を抜き出す(継承)

--- 初級2.5/src/Car.java
+++ 初級2.5/src/Car.java
@@ -1,20 +1,12 @@
 
-public class Car{
-	int x,y,vx,vy;
+public class Car extends Vehicle{
 	public Car(int x,int y,int vx,int vy) {
-		this.x = x;
-		this.y = y;
-		this.vx = vx;
-		this.vy = vy;
+		super(x,y,vx,vy);
 	}
 	public void draw(MyFrame frame) {
 		frame.fillRect(x +20, y, 40, 20);
 		frame.fillRect(x, y + 20, 80, 20);
 		frame.fillOval(x + 10, y + 40, 20, 20);
 		frame.fillOval(x + 50, y + 40, 20, 20);
-	}
-	public void move() {
-		x += vx;
-		y += vy;
 	}
 }
