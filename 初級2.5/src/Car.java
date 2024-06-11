2830d0865663e6fb4af59dcc7a0eab5860325d7f
RYUSEI <utaryu5020@gmail.com>
2024-06-11 09:52:40 +0900

    9-2 キーボードに反応して動くようにする

--- 初級2.5/src/Car.java
+++ 初級2.5/src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle implements KeyListener{
 	
 	public Car(int x,int y,int vx,int vy) {
 		super(x,y,vx,vy);
@@ -10,5 +12,31 @@ public class Car extends Vehicle{
 		frame.fillRect(x, y + 20, 80, 20);
 		frame.fillOval(x + 10, y + 40, 20, 20);
 		frame.fillOval(x + 50, y + 40, 20, 20);
+	}
+
+	//キーから文字が入力されたとき
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	//キーが押されたとき
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			x -= 5;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			x += 5;
+		}
+	}
+
+	//キーが離れたとき
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		x = 0;
 	}
 }
--- 初級2.5/src/PlayerAnimation.java
+++ 初級2.5/src/PlayerAnimation.java
@@ -2,7 +2,8 @@
 public class PlayerAnimation extends MyFrame{
 	
 	public void run() {
-		Car player = new Car(100, 200, 3, 0);
+		Car player = new Car(100, 200, 0, 0);
+		addKeyListener(player);
 		while(true) {
 			clear();
 			player.draw(this);
